8e9235b5ee8a03da4fc7727d61c12bc7401ba79e
yinyongchuan <yinyongchuan@xiaomi.com>
2019-09-26 17:38:08 +0800

    add:MaxHeap

--- MaxHeap.js
+++ MaxHeap.js
@@ -1,30 +1,47 @@
 class MaxHeap{
-    constructor(capacity){
-        this.capacity = this.capacity
+    constructor(){
         this.size = 0
         //0不用
-        this.elements = [null]
+        this.elements = [Infinity]
     }
 
     insert(element){
         this.size++
-        const {size,elements} = this
-        elements[size] = element
-        for(let i=size;i>0&&elements[i]>elements[Math.floor(i/2)];i=Math.floor(i/2)){
-            [elements[i],elements[Math.floor(i/2)]] = [elements[Math.floor(i/2)],elements[i]]
+        const {size,elements: ele} = this
+        ele[size] = element
+        for(let i=size;ele[i]>ele[Math.floor(i/2)];i=Math.floor(i/2)){
+            this._swap(i,Math.floor(i/2))
         }
-        // this.size++
+        console.log(this.elements)
         return this
     }
     _deleteMax(){
-        const {size,elements} = this
-        elements[1] = elements[size]
-        elements.pop()
+        const {size,elements:ele} = this
+        ele[1] = ele[size]
+        ele.pop()
         this.size--
         let i = 1
-        while(typeof elements[i] !== 'undefined'){
-            
+        while(typeof ele[i] !== 'undefined'){
+            let node = ele[i]
+            let left = ele[i*2]
+            let right = ele[i*2+1]
+            if(Math.max(node,left,right)===node){
+                break
+            }
+            if(left>right){
+                this._swap(i,i*2)
+            }else{
+                this._swap(i,i*2+1)
+            }
         }
+    }
+    _swap(left,right){
+        const { elements: ele } = this
+        let tem = ele[left]
+        ele[left] = ele[right]
+        ele[right] = tem
+        // [ele[left] ,ele[right]] = [ele[right] ,ele[left]]
+        return this
     }
     getMax(){
         const max = this.elements[1]
@@ -35,6 +52,4 @@ class MaxHeap{
 
 
 const max = new MaxHeap()
-max.insert(5).insert(2).insert(3).insert(4)
-console.log(max)
-debugger
+max.insert(5).insert(2).insert(3).insert(4).insert(6).getMax()
